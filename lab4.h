9a697534b22afd377f283471cdd910b7629f22b3
Jakub Wieczorek <jakub.wieczorek0101@gmail.com>
2017-05-25 13:17:17 +0200

    Add files via upload

---
+++ lab4.h
@@ -0,0 +1,70 @@
+/*
+ * lab4.h
+ *
+ *  Created on: May 25, 2017
+ *      Author: student
+ */
+
+#include<stdbool.h>
+
+#ifndef HEADERS_LAB4_H_
+#define HEADERS_LAB4_H_
+
+//-----------------------------------------------------------------
+void getRandomData();
+
+void initTimers();
+
+void initSystem();
+
+void prepareCounterB_ForRefresh();
+
+void setStopButton();
+
+void setTimer_A0();
+
+void setTimer_A2();
+
+void resetTimer_A();
+
+void waitForActivation();
+
+void setLED_refresh();
+
+void resetVariables();
+
+void resetTimer_B();
+
+void resetTimer(volatile unsigned int *timer);
+
+void waitForPopButton();
+
+void eliminateOscilations();
+
+//-----------------------------------------------------------------
+volatile int CLOCK_A_HZ;		// Do przechowania aktualnej czestosci zegara
+volatile long CLOCK_B_HZ;
+
+volatile int numOfButton; 	// Numer przycisku, który trzeba wcisnac
+unsigned const int BUTTONS[] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80};	// Kody przycisku do zatrzymywania stopera
+
+volatile long randomTime;	// Losowy czas, po którym odpalamy stoper
+
+volatile int seconds;		// Sekundy liczone przez stoper
+volatile int miliseconds;	// Setne liczone przez stoper
+
+volatile int refresh_id;	// Do wybierania który segment wyswietlacza odswiezamy
+unsigned const int numOfDisplay[] = {0xFE, 0xFD, 0xFB, 0xF7};						// Do wyboru segmentu wyswietlacza
+unsigned const int displayedNum[] = {0xF0, 0xF1, 0xF2, 0xF3, 0xF4, 0xF5, 0xF6, 0xF7, 0xF8, 0xF9};	// Do wyswietlania konkretnego numeru
+unsigned const int displayedNum_dot[] = {0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79};	// Do wyswietlania konkretnego numeru
+volatile int num;		// Zmienna pomocnicza
+
+volatile int TIMER_A_INTERR_MODE;
+volatile int TIMER_B_INTERR_MODE;
+volatile int P1_INTERR_MODE;
+
+volatile bool oscilateFlag = false;
+
+//-----------------------------------------------------------------
+
+#endif /* HEADERS_LAB4_H_ */
