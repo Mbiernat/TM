3462cf66d901d7762658afe713b161401febb0c7
Mbiernat <mati.biernat@wp.pl>
2017-06-09 00:02:22 +0200

    Create lab4.h

--- Lab4/Ostateczne_pliki/Nowy_wyswietlacz/lab4.h
+++ Lab4/Ostateczne_pliki/Nowy_wyswietlacz/lab4.h
@@ -48,7 +48,6 @@ volatile int refresh_id;// Do wybierania który segment wyswietlacza odswiezamy
 volatile int word_id;// Do wybierania !!!!
 unsigned const int numOfDisplay[] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80};						// Do wyboru segmentu wyswietlacza
 unsigned const int displayedSymb[] = {0x7E, 0x30, 0x6D, 0x79, 0x33, 0x5B, 0x5F, 0x70, 0x7F, 0x7B, 0x00, 0x7B, 0x7E, 0x3D, 0x1F,  0x77, 0x1C, 0x67};	// Do wyswietlania konkretnego numeru //0-9 <none> g o d b a u p
-unsigned const int displayLooser[] = {};
 volatile int num;		// Zmienna pomocnicza
 
 volatile int TIMER_A_INTERR_MODE;
